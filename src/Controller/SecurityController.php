e01fcb4aa22313925dcb6b9e99dbc16f3423ff56
escees <kamil.scislowski@xsolve.pl>
2019-07-02 21:32:28 +0200

    security and logging fixes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -17,6 +17,17 @@ class SecurityController extends AbstractController
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+                'last_username' => $lastUsername,
+                'error' => $error
+            ]
+        );
+    }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout(): void
+    {
     }
 }
