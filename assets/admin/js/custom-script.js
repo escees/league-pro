6c37219f69e41d6c900b435ce6c566e06044e6c8
Kamil Ścisłowski <kamil.scislowski@sts.pl>
2019-07-13 22:15:42 +0200

    fixed date picker, added fixtures bundle

--- assets/admin/js/custom-script.js
+++ assets/admin/js/custom-script.js
@@ -9,3 +9,8 @@ NOTE:
 ------
 PLACE HERE YOUR OWN JS CODES AND IF NEEDED.
 WE WILL RELEASE FUTURE UPDATES SO IN ORDER TO NOT OVERWRITE YOUR CUSTOM SCRIPT IT'S BETTER LIKE THIS. */
+$(document).ready(function () {
+    $('.datepicker').on('mousedown', function(event) {
+        event.preventDefault();
+    });
+});
--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
