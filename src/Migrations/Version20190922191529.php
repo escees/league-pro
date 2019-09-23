435bfff44da9113bee6e6c22f057e8c2620318a2
Kamil Ścisłowski <kamil.scislowski@sts.pl>
2019-09-23 19:20:02 +0200

    mvp-and-result-page-logos: removed comments from migration

--- src/Migrations/Version20190922191529.php
+++ src/Migrations/Version20190922191529.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20190922191529 extends AbstractMigration
 {
     public function getDescription() : string
@@ -19,7 +16,6 @@ final class Version20190922191529 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE man_of_the_match (id INT AUTO_INCREMENT NOT NULL, player_id INT NOT NULL, match_details_id INT DEFAULT NULL, INDEX IDX_D21F920599E6F5DF (player_id), UNIQUE INDEX UNIQ_D21F9205FA07CC0E (match_details_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
@@ -29,7 +25,6 @@ final class Version20190922191529 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE man_of_the_match');
