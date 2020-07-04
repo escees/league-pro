635436dd0b7880a98da2e5b8ff12780a47540d3e
Kamil Ścisłowski <kamil.scislowski@sts.pl>
2020-07-04 18:12:03 +0200

    feature(news): sorting and displaying news date on homepage

---
+++ src/Migrations/Version20200704155621.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200704155621 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE news ADD created_at DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE news SET created_at = NOW()');
+        $this->addSql('ALTER TABLE news change created_at created_at DATETIME NOT NULL');
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE news DROP created_at');
+    }
+}
