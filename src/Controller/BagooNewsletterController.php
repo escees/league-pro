40cb6cf5f4f84cd42c86b818d9a4121e4ba98a30
Kamil Ścisłowski <kamil@blossomeducational.com>
2023-11-18 19:46:01 +0100

    update php and symfony + newsletter proxy endpoint

--- src/Controller/BagooNewsletterController.php
+++ src/Controller/BagooNewsletterController.php
@@ -11,6 +11,7 @@ use Brevo\Client\Model\CreateContact;
 use Exception;
 use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,25 +20,21 @@ class BagooNewsletterController extends AbstractController
     /**
      * @Route("/newsletter", name="app.bagoo.newsletter")
      */
-    public function index(): Response
-    {
-        // Configure API key authorization: api-key
-        $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', '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');
-
-// Uncomment below line to configure authorization using: partner-key
-// $config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('partner-key', 'YOUR_API_KEY');
-
+    public function index(
+        Request $request,
+        string $brevoApiKey
+    ): Response {
+        $config = Configuration::getDefaultConfiguration()->setApiKey('api-key', $brevoApiKey);
         $apiInstance = new ContactsApi(
-        // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
-        // This is optional, `GuzzleHttp\Client` will be used as default.
             new Client(),
             $config
         );
-        $createContact = new CreateContact(); // \SendinBlue\Client\Model\CreateContact | Values to create a contact
+        $decodedParams = json_decode($request->getContent());
+        $createContact = new CreateContact();
 
-        $createContact['email'] = 'kamil.scislowski87+114@gmail.com';
-        $createContact['attributes'] = array('FNAME'=>'Kamil', 'LNAME'=>'Ścisłowski');
-        $createContact['listIds'] = array(4);
+        $createContact['email'] = $decodedParams['email'];
+        $createContact['attributes'] = ['FNAME'=> $decodedParams['firstName'], 'LNAME'=>$decodedParams['lastname']];
+        $createContact['listIds'] = [4];
         $createContact['emailBlacklisted'] = false;
         $createContact['smsBlacklisted'] = false;
         $createContact['updateEnabled'] = false;
